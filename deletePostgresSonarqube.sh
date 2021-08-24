0573e143b2f1db3b3ae6a7f11479f8851e49fd21
Andrés <andres.pinzon@jala-foundation.org>
2021-08-24 07:37:01 -0500

    added postgres container creation and connection command

--- deletePostgresSonarqube.sh
+++ deletePostgresSonarqube.sh
@@ -1,6 +1,6 @@
 #delete volumes
 docker volume rm postgres-data
 #deletecontainers
-
+docker rm -v ap-postgres #-v is redundant with volume rm command
 #delete subnetwork
 docker network rm postgres-sonarqube
--- scriptPostgresSonarqube.sh
+++ scriptPostgresSonarqube.sh
@@ -3,3 +3,6 @@ docker network create --driver bridge postgres-sonarqube
 #create volumes
 docker volume create postgres-data
 #create containers
+docker run --name ap-postgres -e POSTGRES_PASSWORD=changeThisPWD -d postgres -v postgres-data:/var/lib/postgresql/data
+#connect containers
+docker network connect postgres-sonarqube ap-postgres
