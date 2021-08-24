5d08efa46ca681b0e6b98cb8e004ca861d608a07
Andrés <andres.pinzon@jala-foundation.org>
2021-08-24 08:16:48 -0500

    added publish to postgres command, changed deletion order script

--- deletePostgresSonarqube.sh
+++ deletePostgresSonarqube.sh
@@ -1,7 +1,7 @@
-#delete volumes
-docker volume rm postgres-data
 #deletecontainers
 docker rm -v -f ap-sonarqube
 docker rm -v -f ap-postgres #in case postgres made any extra volumes
+#delete volumes
+docker volume rm postgres-data
 #delete subnetwork
 docker network rm postgres-sonarqube
