1e34cdf4310d87ad101173a45217784c20aa9a39
Andrés <andres.pinzon@jala-foundation.org>
2021-08-24 12:14:45 -0500

    Fixed script typo in nexus container name. Added jenkins and nexus to delete script

--- deletePostgresSonarqube.sh
+++ deletePostgresSonarqube.sh
@@ -1,7 +1,12 @@
 #deletecontainers
 docker rm -v -f ap-sonarqube
 docker rm -v -f ap-postgres #in case postgres made any extra volumes
+docker rm -v -f ap-jenkins
+docker rm -v -f ap-nexus3
 #delete volumes
 docker volume rm postgres-data
+docker volume rm sonarqube-data
+docker volume rm jenkins-data
+docker volume rm nexus3-data
 #delete subnetwork
 docker network rm postgres-sonarqube
--- scriptPostgresSonarqube.sh
+++ scriptPostgresSonarqube.sh
@@ -13,4 +13,4 @@ docker run --name ap-sonarqube -p 9000:9000 --network postgres-sonarqube -d sona
 #create and connect jenkins container
 docker run --name ap-jenkins -p 8080:8080 --network postgres-sonarqube -d jenkins/jenkins
 #create and connect sonatype/nexus container
-docker run --name ap-sonarqube -p 8081:8081 --network postgres-sonarqube -d sonatype/nexus3
+docker run --name ap-nexus3 -p 8081:8081 --network postgres-sonarqube -d sonatype/nexus3
