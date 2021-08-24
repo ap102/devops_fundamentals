f86447322ecadf6bcb16f67030961d707faa977c
Andrés <andres.pinzon@jala-foundation.org>
2021-08-24 12:06:27 -0500

    added containers and volumes for jenkins and nexus. Volumes not linked yet

--- scriptPostgresSonarqube.sh
+++ scriptPostgresSonarqube.sh
@@ -2,8 +2,15 @@
 docker network create --driver bridge postgres-sonarqube
 #create volumes
 docker volume create postgres-data
+docker volume create sonarqube-data
+docker volume create jenkins-data
+docker volume create nexus3-data
 #create and connect postgres container
 docker run --name ap-postgres -e POSTGRES_PASSWORD=changeThisPWD -p 5432:5432 -v postgres-data:/var/lib/postgresql/data -d postgres
 docker network connect postgres-sonarqube ap-postgres
 #create and connect sonarqube container
 docker run --name ap-sonarqube -p 9000:9000 --network postgres-sonarqube -d sonarqube
+#create and connect jenkins container
+docker run --name ap-jenkins -p 8080:8080 --network postgres-sonarqube -d jenkins/jenkins
+#create and connect sonatype/nexus container
+docker run --name ap-sonarqube -p 8081:8081 --network postgres-sonarqube -d sonatype/nexus3
