e43a889015af580d3cf988dfe386c457a8963641
Andrés <andres.pinzon@jala-foundation.org>
2021-08-24 12:32:19 -0500

    linked volume nexus3-data to container in /nexus-data

--- scriptPostgresSonarqube.sh
+++ scriptPostgresSonarqube.sh
@@ -13,4 +13,4 @@ docker run --name ap-sonarqube -p 9000:9000 --network postgres-sonarqube -d sona
 #create and connect jenkins container
 docker run --name ap-jenkins -p 8080:8080 -p 50000:50000 -v jenkins-workspace:/var/jenkins_home --network postgres-sonarqube -d jenkins/jenkins
 #create and connect sonatype/nexus container
-docker run --name ap-nexus3 -p 8081:8081 --network postgres-sonarqube -d sonatype/nexus3
+docker run --name ap-nexus3 -p 8081:8081 -v nexus3-data:/nexus-data --network postgres-sonarqube -d sonatype/nexus3
